03960870de34f2b67170760537d28a9ff43d9fb7
Smad122 <96110858+Smad122@users.noreply.github.com>
2024-05-03 17:47:53 +0700

    Add files via upload

---
+++ CG_RGZ/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CG_RGZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(16, 585);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1480, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press \'Load...\' to open any polygon file supported by SharpGL.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Wavefont Obj Files (*.obj)|*.obj|All Supported File Types|*.scn;*.cob;*.3ds;*.sgl" +
+    "sx;*.obj;";
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLoad.Location = new System.Drawing.Point(1396, 623);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(100, 28);
+            this.buttonLoad.TabIndex = 4;
+            this.buttonLoad.Text = "Load...";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.button2);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.button1);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.openGLControl1);
+            this.toolStripContainer1.ContentPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1512, 666);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(1512, 666);
+            this.toolStripContainer1.TabIndex = 5;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(1334, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Освещение";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(1334, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.FrameRate = 60;
+            this.openGLControl1.Location = new System.Drawing.Point(-5, -1);
+            this.openGLControl1.Margin = new System.Windows.Forms.Padding(5);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.NativeWindow;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(1519, 802);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.OpenGLInitialized += new System.EventHandler(this.openGLControl1_OpenGLInitialized);
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            this.openGLControl1.Load += new System.EventHandler(this.openGLControl1_Load);
+            this.openGLControl1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dmove);
+            this.openGLControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1512, 666);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.MouseEnter += new System.EventHandler(this.mouse_resize);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ CG_RGZ/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using SharpGL;
+using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Primitives;
+using SharpGL.Serialization;
+using SharpGL.SceneGraph.Core;
+using SharpGL.Enumerations;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CG_RGZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private float _rotateX = 0;
+        private float _rotateY = 0;
+        private float _rotateZ = 0;
+
+        private int _moveX = 0;
+        private int _moveY = 0;
+
+        private float _scaleFactor = 1;
+        private float _scaleDelta = 1;
+        private float _scaleDefault = 1;
+
+        Polygon obj = null;
+       
+       
+
+
+        private void openGLControl1_OpenGLDraw(object sender, RenderEventArgs e)
+        {
+            if (obj != null) { 
+                OpenGL gl = openGLControl1.OpenGL;
+
+                gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+                gl.LoadIdentity();
+                gl.LookAt(10, 10, 10, 0, 0, 0, 0, 1, 0);
+
+                gl.Rotate(_rotateX, 1.0f, 0.0f, 0.0f);
+                gl.Rotate(_rotateY, 0.0f, 1.0f, 0.0f);
+                gl.Rotate(_rotateZ, 0.0f, 0.0f, 1.0f);
+            
+                gl.Translate(_moveX, 0, 0);
+                gl.Translate(0,_moveY,0);
+            
+                obj.Transformation.ScaleX = _scaleFactor;
+                obj.Transformation.ScaleY = _scaleFactor;
+                obj.Transformation.ScaleZ = _scaleFactor;
+                obj.PushObjectSpace(gl);
+                obj.Render(gl, RenderMode.Render);
+                obj.PopObjectSpace(gl);
+            }
+        }
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {           
+            openGLControl1.OpenGL.PolygonMode(FaceMode.FrontAndBack, PolygonMode.Lines);
+            
+        }
+      
+     
+        int predx = 0;
+        int predy = 0;
+        private void dmove(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == 's')|| (e.KeyChar == 'ы'))
+            {
+                _moveY -= 1;
+            }
+            if ((e.KeyChar == 'a') || (e.KeyChar == 'ф'))
+            {
+                _moveX -= 1;
+            }
+            if ((e.KeyChar == 'd') || (e.KeyChar == 'в'))
+            {
+                _moveX += 1;
+            }
+            if ((e.KeyChar == 'w') || (e.KeyChar == 'ц'))
+            {
+                _moveY += 1;
+            }
+            if ((e.KeyChar == 'z') || (e.KeyChar == 'я'))
+            {
+                _scaleFactor += (float)0.1;
+            }
+            if ((e.KeyChar == 'c') || (e.KeyChar == 'с'))
+            {
+                _scaleFactor -= (float)0.1;
+            }
+        }
+       
+        
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {//Обработчик события перемещения мыши по pictuteBox1
+            if (e.Button == MouseButtons.Left) //Проверяем нажата ли левая кнопка мыши
+            { //запоминаем в point текущее положение курсора мыши
+                if ((predx == 0) && (predy == 0))
+                {
+                    predx = e.X;
+                    predy = e.Y;
+                }
+                else
+                {
+                    int pointX = e.X;
+                    int pointY = e.Y;
+                    //if (pointX - predx > 0)
+                    //{
+                    //    if (pointY - pointY > 0)
+                    //        _rotateX += (int)Math.Sqrt(Math.Pow((pointY - predy), 2) + Math.Pow((pointX - predx), 2));
+                    //    else _rotateZ += (int)Math.Sqrt(Math.Pow((-pointY + predy), 2) + Math.Pow((-pointX + predx), 2));
+
+                    //}
+
+                    if (pointX-predx<=0)
+                    {
+                        _rotateY+= pointX - predx;
+                    }
+                   if (pointX - predx > 0)
+                    {
+                        _rotateY -= predx-pointX;
+                    }
+                 
+                        if (pointX- predx>0)
+                        {
+                            _rotateX += pointY - predy;
+
+                        }
+                        if (pointX - predx <0)
+                        {
+                            _rotateZ += pointY - predy;
+
+                       }
+                    if (pointX- predx==0)
+                    {
+                        _rotateX += pointY - predy;
+                        _rotateZ += pointY - predy;
+                    }
+           
+
+                
+                    predx = pointX;
+                    predy = pointY;
+                }
+            }
+            else
+            {
+                predx = 0;
+                predy = 0;
+            }
+           
+
+
+        }
+ 
+      
+
+        private void openGLControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mouse_resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = ("obj файлы(*.obj) | *.obj");
+            if (openDialog.ShowDialog() != DialogResult.OK) return;
+            var scene = SerializationEngine.Instance.LoadScene(openDialog.FileName);
+            obj = scene.SceneContainer.Children.OfType<Polygon>().FirstOrDefault();
+
+            float[] borders = new float[3];
+            obj.BoundingVolume.GetBoundDimensions(out borders[0], out borders[1], out borders[2]);
+            float maxBorder = borders.Max();
+            float scaleFactor = maxBorder > 10 ? 10.0f / maxBorder : 1;
+            obj.Transformation.ScaleX = scaleFactor;
+            obj.Transformation.ScaleY = scaleFactor;
+            obj.Transformation.ScaleZ = scaleFactor;
+            marker = true;
+            _scaleFactor = scaleFactor;
+            _scaleDefault = scaleFactor;
+            _scaleDelta = scaleFactor / 10;
+            openGLControl1.OpenGL.PolygonMode(FaceMode.FrontAndBack, PolygonMode.Lines);
+            openGLControl1.OpenGL.Disable(OpenGL.GL_LIGHTING);
+        }
+        bool marker = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (marker == true)
+            {
+                openGLControl1.OpenGL.PolygonMode(FaceMode.FrontAndBack, PolygonMode.Filled);
+                openGLControl1.OpenGL.Enable(OpenGL.GL_LIGHTING);
+               openGLControl1.OpenGL.Enable(OpenGL.GL_LIGHT0);
+            
+                openGLControl1.OpenGL.Enable(OpenGL.GL_DEPTH_TEST);
+            }
+        }
+    }    
+}
